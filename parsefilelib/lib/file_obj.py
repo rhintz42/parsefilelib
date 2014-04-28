56f599e8118659d82d25687c4fd3f95036fdf0f3
Robert Hintz <roberth@surveymonkey.com>
2014-04-27 17:38:38 -0700

    Add docstrings to library files for the FileObj and FolderObj classes

--- parsefilelib/lib/file_obj.py
+++ parsefilelib/lib/file_obj.py
@@ -1,2 +1,5 @@
 def get_folder_path_from_file_path(file_path):
+    """
+    Return the folder path given a file's path
+    """
     return '/'.join(file_path.split('/')[:-1])
--- parsefilelib/lib/folder_obj.py
+++ parsefilelib/lib/folder_obj.py
@@ -2,6 +2,9 @@ from os import listdir
 from os.path import isdir, isfile
 
 def get_child_file_names(folder_path):
+    """
+    Return a list of names of files that are contained within the folder path
+    """
     file_names_in_folder = []
     try:
         for f in listdir(folder_path):
@@ -14,6 +17,9 @@ def get_child_file_names(folder_path):
     return file_names_in_folder
 
 def get_child_folder_names(folder_path):
+    """
+    Return a list of names of folders that are contained within the folder path
+    """
     folder_names_in_folder = []
     try:
         for f in listdir(folder_path):
@@ -26,6 +32,12 @@ def get_child_folder_names(folder_path):
     return folder_names_in_folder
 
 def format_folder_path(folder_path):
+    """
+    Return the folder path correctly formatted for the FolderObj class
+
+    Note: It is necessary to correctly format the folder_path string in order
+        for you to call certain methods, like getting the folder name
+    """
     if folder_path[-1] != '/':
         folder_path += '/'
 
